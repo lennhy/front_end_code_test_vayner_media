742dd15cde537c55db5bb58de8d71f1c7409ef4d
lenn hypolite <lennhydesign@gmail.com>
2017-04-19 13:28:37 -0400

    Ui update of promise data to the dom and removal of old data from the dom completed

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
     function getAlbums(){
       var user1Albums=[];
       var user2Albums=[];
+      //  Get request 1
       $.get( albums, function(data) {
       })
       .done(function(albumObj) {
@@ -39,55 +40,87 @@ $(document).ready(function(){
 
     // Get single user getAlbumslbums
     function getUser(user,id, userAlbums) {
+      var albumId;
+      var userId;
+      var title;
+
       // The data will return only when all the object values are returned for each promise.
+      // Get request 2
        $.get( user+id, function(data) {
        })
        .done(function(albumObj) {
          var name = '<div class="user-name">user name: '+albumObj[0].name+'</div>';
-         $('.container').append(name);
+         $(`.user-table-${id}`).append(name);
          for(var i=0; i < userAlbums.length; i++){
-           $('.container').append(
-             '<div class="user-table">'+
-               `<ul id="album-row-${userAlbums[i].id}"` + " "+  'draggable="true">'+
-                 '<li class="id-column"> album Id: '+userAlbums[i].id+'</li>'+
-                ' <li class="title-column"> album Title: '+userAlbums[i].title+'</li>'+
-              '</ul>'+
-             '</div>'
-           )
+           if(id===1){
+             $('.user-table-1').append(
+                 `<ul id="album-row-${userAlbums[i].id}"` + " "+  'draggable="true">'+
+                   '<li class="id-column"> album Id: '+userAlbums[i].id+'</li>'+
+                  ' <li class="title-column"> album Title: '+userAlbums[i].title+'</li>'+
+                '</ul>'
+             )
+           }
+           if(id===2){
+             $('.user-table-2').append(
+                 `<ul id="album-row-${userAlbums[i].id}"` + " "+  'draggable="true">'+
+                   '<li class="id-column"> album Id: '+userAlbums[i].id+'</li>'+
+                  ' <li class="title-column"> album Title: '+userAlbums[i].title+'</li>'+
+                '</ul>'
+             )
+           }
          }
        })
        .fail(function() {
          console.log( "error" );
        });
+
+       // -------------------------------------------- DRAG AND DROP FUNCTIONALITY
+
+       document.addEventListener("dragstart", function(event) {
+           event.dataTransfer.setData("Text", event.target.id);
+       });
+
+        document.addEventListener("dragover", function(event) {
+           event.preventDefault();
+        });
+
+        document.addEventListener("drag", function(event){
+           event.dataTransfer.setData("text", event.target.id);
+           albumId = event.target.id.slice(10,12);
+           title = event.target.children[1].innerHTML;
+        });
+
+        document.addEventListener("drop", function(event) {
+           event.preventDefault();
+           var dropContainer = $(event.target);
+           if ( event.target.className == `user-table-${id}` ) {
+             userId  = event.target.className.slice(11,12);
+             var albumToTransfer = event.dataTransfer.getData("text");
+
+             // Update the user id of the album
+             $.ajax({
+                 type: "PUT",
+                 url: albums+"/"+albumId,
+                 data: {
+                   userId: userId,
+                   id: albumId,
+                   title: title
+                   }
+              })
+              .done(function(album ) {
+                   // After request resolves the album from the original user table will be removed
+                   $(document.getElementById(albumToTransfer)).remove();
+                   // After request resolves the album data returned from the promise will be appended to the new user's table
+                   dropContainer.append(
+                     `<ul id="${album.id}" `+  'draggable="true">'+
+                       '<li class="id-column"> album Id: '+ album.id+'</li>'+
+                      ' <li class="title-column"> album Title: '+album.title+'</li>'+
+                    '</ul>'
+                   );
+              });
+            }
+        });
      }
-
-      // -------------------------------------------- DRAG AND DROP FUNCTIONALITY
-
-      document.addEventListener("dragstart", function(event) {
-          console.log("dragstart");
-          event.dataTransfer.setData("Text", event.target.id);
-      });
-
-       document.addEventListener("dragover", function(event) {
-          console.log("dragover");
-          event.preventDefault();
-       });
-
-       document.addEventListener("drag", function(event){
-          event.dataTransfer.setData("text", event.target.id);
-       });
-
-       document.addEventListener("drop", function(event) {
-          event.preventDefault();
-          console.log("drop");
-          console.log(event.target.className);
-
-          if ( event.target.className == "user-table" ) {
-              var data = event.dataTransfer.getData("Text");
-              console.log(data);
-              event.target.appendChild(document.getElementById(data));
-          }
-       });
 
 
 });
